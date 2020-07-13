a52f71928532bc114a3cbc6adfeacfd6fc4e7fdb
Gheorghe Sacultanu <sacultanugheorghe@gmail.com>
2020-07-13 16:32:20 +0200

    Added the headless mode

--- features/step_definitions/REQ-UI-01_global_checks_home_step_defs.js
+++ features/step_definitions/REQ-UI-01_global_checks_home_step_defs.js
@@ -3,7 +3,18 @@ const { Given, When, Then } = require('cucumber');
 const { Builder, By, until } = require('selenium-webdriver');
 const { genericPageObjects } = require('../../page-objects/generic-elements.js');
 
-let driver = new Builder().forBrowser('chrome').build();
+// let driver = new Builder().forBrowser('chrome').build();
+
+var seleniumWebdriver = require('selenium-webdriver');
+var chrome    = require('selenium-webdriver/chrome');
+
+var options   = new chrome.Options().headless();
+
+var driver = new seleniumWebdriver.Builder()
+  .forBrowser('chrome')
+  .setChromeOptions(options)
+  .build()
+
 
 
   Given('Url {string}', async function (String) {
--- features/step_definitions/REQ-UI-04_home_page_step_defs.js
+++ features/step_definitions/REQ-UI-04_home_page_step_defs.js
@@ -1,10 +1,20 @@
 const assert = require('assert');
 const { Given, When, Then } = require('cucumber');
-const { Builder, By, until } = require('selenium-webdriver');
+const { until } = require('selenium-webdriver');
 const { navigationBarPageObjects } = require('../../page-objects/navigatio-bar-page.js');
 
 
-let driver = new Builder().forBrowser('chrome').build();
+
+var seleniumWebdriver = require('selenium-webdriver');
+var chrome    = require('selenium-webdriver/chrome');
+
+var options   = new chrome.Options().headless();
+
+var driver = new seleniumWebdriver.Builder()
+  .forBrowser('chrome')
+  .setChromeOptions(options)
+  .build()
+
 
 Given('Open URL {string}', async function (string) {
   await driver.get(string);
